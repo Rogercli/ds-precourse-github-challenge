9df022cf568d2cacd0684ef96a93d7277987e05c
Roger Li <Rogerli1992@gmail.com>
2021-07-24 16:45:44 -0700

    distributions.py challenge submission 1

--- distributions.py
+++ distributions.py
@@ -1,13 +1,23 @@
 import math
+from math import e
 
+def factorial(n):
+    prod=1
+    for i in range(2,n+1):
+        prod*=i
+    return prod
 
+def combinations(n,k):
+    return factorial(n)/(factorial(n-k)*factorial(k))
 def binomial_pmf(n, p, k):
-    pass
+    x=combinations(n,k)
+    return (x*(((p)**k)*((1-p)**(n-k))))
 
 
-def poisson_pmf(k, lam):
-    pass
+
+def poisson_pmf(k,lam):
+    return ((lam**k)*(e**-lam))/factorial(k)
 
 
-def geometric_pmf(n, p):
-    pass
+def geometric_pmf(n,p):
+    return (1-p)**(n-1)*p
